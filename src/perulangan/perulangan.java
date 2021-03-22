7dbe46a941926290740c90e1692b458c9bbaae6f
Raha <erbees.50000@gmail.com>
2021-03-22 14:49:45 +0700

    fixed number 0

--- src/perulangan/perulangan.java
+++ src/perulangan/perulangan.java
@@ -4,12 +4,12 @@ public class perulangan {
     public static void main(String[] args) {
         int x,y,z;
         
-        for ( x = 11; x >= 2; x--) {
-            for ( y = 11; y > x; y--) {
+        for ( x = 10; x >=1; x--) {
+            for ( y = 10; y > x; y--) {
                 System.out.print(" ");
             }
-            for ( z = 1; z < (x * 1); z++) {
-                System.out.print(z + " ");
+            for ( z = 1; z <= (x * 1); z++) {
+                System.out.print(z % 10 + " ");
             }
            System.out.println();
         }
